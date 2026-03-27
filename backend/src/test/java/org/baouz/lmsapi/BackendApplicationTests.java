0148fbf09e9ebd4f33e725509430ce9ccd7f6293
Sifeddine <sifeddinebaouz@gmail.com>
2026-03-27 16:28:58 +0100

    First commit: Git repo initialization

---
+++ backend/src/test/java/org/baouz/lmsapi/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.baouz.lmsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
